347552b86902cc3b4704ac253b6e4dcfae393557
khaeru99 <khaerul.r.23@gmail.com>
2025-11-11 16:28:42 +0700

    update services

--- src/App.jsx
+++ src/App.jsx
@@ -116,7 +116,7 @@ function App() {
             {/* about */}
             <section
               id="about"
-              className="min-h-screen flex justify-center items-center px-4 md:py-20 relative"
+              className="min-h-screen flex justify-center items-center px-4 py-20 relative"
             >
               <div className="flex justify-center items-center border border-gray-300 dark:border-gray-700 w-full bg-white dark:bg-[#284b63] py-20 shadow-xl shadow-gray-300 dark:shadow-gray-900/50 rounded-2xl">
                 <div className="grid grid-cols-1 md:grid-cols-2 place-items-center">
@@ -132,7 +132,7 @@ function App() {
                           <li className="font-bold">
                             <a
                               href="https://wa.me/6289530185171"
-                              className="text-[#d9d9d9] hover:text-green-400 transition-all duration-150"
+                              className="text-gray-400 dark:text-[#d9d9d9] hover:text-green-400 transition-all duration-150"
                             >
                               <FaWhatsappSquare className="h-8 w-8 hover:scale-110 transition-all duration-150" />
                             </a>
@@ -140,7 +140,7 @@ function App() {
                           <li className="font-bold">
                             <a
                               href="https://wa.me/6289530185171"
-                              className="text-[#d9d9d9] hover:text-blue-800 transition-all duration-150"
+                              className="text-gray-400 dark:text-[#d9d9d9] hover:text-blue-800 transition-all duration-150"
                             >
                               <FaLinkedin className="h-8 w-8 hover:scale-110 transition-all duration-150" />
                             </a>
@@ -148,7 +148,7 @@ function App() {
                           <li className="font-bold">
                             <a
                               href="https://wa.me/6289530185171"
-                              className="text-[#d9d9d9] hover:text-pink-500 transition-all duration-150"
+                              className="text-gray-400 dark:text-[#d9d9d9] hover:text-pink-500 transition-all duration-150"
                             >
                               <FaInstagram className="h-8 w-8 hover:scale-110 transition-all duration-150" />
                             </a>
@@ -156,7 +156,7 @@ function App() {
                           <li className="font-bold">
                             <a
                               href="https://wa.me/6289530185171"
-                              className="text-[#d9d9d9] hover:text-blue-500 transition-all duration-150"
+                              className="text-gray-400 dark:text-[#d9d9d9] hover:text-blue-500 transition-all duration-150"
                             >
                               <FaFacebookSquare className="h-8 w-8 hover:scale-110 transition-all duration-150" />
                             </a>
@@ -202,18 +202,18 @@ function App() {
             {/* services */}
             <section
               id="services"
-              className="pt-20 items-center min-h-screen bg-gray-100 dark:bg-gray-400 w-screen relative left-1/2 -translate-x-1/2 scroll-mt-20"
+              className="py-20 items-center min-h-screen bg-gray-100 dark:bg-gray-400 w-screen relative left-1/2 -translate-x-1/2 scroll-mt-20"
             >
               <div className="max-w-7xl mx-auto p-4">
                 <div className="text-center flex flex-col justify-center items-center mb-20">
-                  <p className="text-lg font-medium text-blue-400">services</p>
-                  <h2 className="text-4xl font-bold mb-4 text-black ">
+                  <p className="text-lg font-medium text-[#284b63] dark:text-gray-300">services</p>
+                  <h2 className="text-4xl font-bold mb-4 text-black dark:text-white">
                     Service I Provide
                   </h2>
                   <div className="flex gap-1">
-                    <div className="w-20 bg-blue-400 h-1 rounded-2xl" />
-                    <div className="w-10 bg-blue-400 h-1 rounded-2xl" />
-                    <div className="w-5 bg-blue-400 h-1 rounded-2xl" />
+                    <div className="w-20 bg-[#284b63] h-1 rounded-2xl" />
+                    <div className="w-10 bg-[#284b63] h-1 rounded-2xl" />
+                    <div className="w-5 bg-[#284b63] h-1 rounded-2xl" />
                   </div>
                 </div>
 
@@ -223,7 +223,7 @@ function App() {
 
                     {dataServices.map((service) => (
                       <div key={service.id}>
-                        <div className="bg-white dark:bg-gray-700 p-6 rounded-2xl shadow-lg shadow-gray-300 hover:scale-105 transition-all duration-150">
+                        <div className="bg-white dark:bg-gray-700 p-6 rounded-2xl shadow-lg shadow-gray-300 dark:shadow-gray-400 hover:scale-105 transition-all duration-150">
                           <h3 className="text-xl font-bold mb-4">
                             {service.title}
                           </h3>
